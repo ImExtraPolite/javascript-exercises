8ef6743c4f619e07ba358ea6e4695bd55dff8a33
ImExtraPolite <jaleel1666@gmail.com>
2023-06-14 20:23:45 -0400

    done with 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let newWord = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    newWord = newWord + word[i];
+  }
+
+  return newWord;
 
 };
 
