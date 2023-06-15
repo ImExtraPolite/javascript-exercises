787a43bbf545f7dc1f05292b651e131c1895adf7
ImExtraPolite <jaleel1666@gmail.com>
2023-06-14 20:03:17 -0400

    yes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+  let newWord = "";
+  for (let i = 0; i < num; i++) {
+    if (num >= 0) {
+      newWord = newWord + word;
+    } else {
+      newWord == "ERROR";
+    }
+  }
 
+  return newWord;
 };
 
 // Do not edit below this line
